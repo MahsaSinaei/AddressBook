c18d74008b3f9093e8c8ff61c40df84f0593e320
mahsasinaei <mah.sinaei@gmail.com>
2022-09-19 00:39:20 -0400

    -m My second commit

---
+++ src/AddressBook.java
@@ -0,0 +1,20 @@
+public class AddressBook {
+    BuddyInfo Homer =new BuddyInfo("Homer");
+    BuddyInfo Ali =new BuddyInfo("Ali");
+    BuddyInfo Babak =new BuddyInfo("Babak");
+    BuddyInfo Mahsa =new BuddyInfo("Mahsa");
+
+    public void addBuddy(String aBuddy){
+
+    }
+
+    public void  removeBuddy(String aBuddy){
+
+    }
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Jhon");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
+    }
+}
