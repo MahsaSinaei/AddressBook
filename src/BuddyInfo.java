6ea2271b10dcb407ea39968ec8aa271f6d738c64
mahsasinaei <mah.sinaei@gmail.com>
2022-09-24 17:42:07 -0400

    -m My BuddyInfo

---
+++ src/BuddyInfo.java
@@ -0,0 +1,17 @@
+
+public class BuddyInfo {
+
+    // Instance Variable
+    private final String name;
+
+    // Constructor Declaration
+    public BuddyInfo (String name) {
+        this.name = name;
+    }
+
+    // Getter method
+    public String getName() {
+        return name;
+    }
+
+}
